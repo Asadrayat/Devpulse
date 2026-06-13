99feac9e111c44ea4a7dd03e87dea25ecd46e180
Asad Rayat <asadrayat@Asads-MacBook-Air.local>
2026-06-14 05:23:15 +0600

    removed more  unnnecessary comment

--- src/app.ts
+++ src/app.ts
@@ -8,21 +8,18 @@ import express, {
 import { userRoute } from "./modules/user/user.route";
 
 import { authRoute } from "./modules/auth/auth.route";
-// import logger from "./middleware/logger";
-// import CookieParser from "cookie-parser";
+
 import cors from "cors";
-// import cors from "cors";
-// import globalErrorHandler from "./middleware/globalErrorHandler";
+
 import { issueRoute } from "./modules/issues/issues.route";
 import globalErrorHandler from "./middleware/globalErrorHandler";
 
 const app: Application = express();
 
-// app.use(CookieParser()); // to refresh token to the controller
+
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
-// app.use(logger);
 
 const corsOption = {
     origin: "http://localhost:8000/",    
@@ -31,19 +28,18 @@ const corsOption = {
 app.use(cors(corsOption))
 
 app.get("/", (req: Request, res: Response) => {
-    // res.send('Hello World User!');
+   
     res.status(200).json({
         message: "Devpulse Server",
         author: "Devpulse",
     });
 });
 
-app.use("/api/auth", userRoute); // /api/auth/signup route will create user
+app.use("/api/auth", userRoute); 
 app.use("/api/issues", issueRoute);
 app.use(globalErrorHandler);
 app.use("/api/auth", authRoute);
 
-// app.use(globalErrorHandler);
 
 
 export default app;
